c86657b26d7442e8c4dab27a5a2b8aee36f2500c
wow1548 <wow1548@naver.com>
2020-02-22 16:53:35 +0900

    style login form

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,10 +3,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import Chat from "./components/Chat";
 
 const BaseRouter = () => (
   <Switch>
-    <Route path="" component={Login} />
+    <Route exact path="/" component={Login} />
+    <Route path="/chat" component={Chat} />
   </Switch>
 );
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Axios from "axios";
+import styled, { keyframes } from "styled-components";
 
 const LoginContainer = () => {
   const [loginForm, setLoginForm] = useState(true);
@@ -45,32 +46,143 @@ const LoginContainer = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
+    <LoginDiv>
+      Login
+      <LoginForm onSubmit={onSubmit}>
         {loginForm ? (
           <div>
-            <input type="text" name="username" placeholder="username" />
-            <input type="password" name="password" placeholder="password" />
+            <p>
+              <input
+                type="text"
+                autoComplete="off"
+                name="username"
+                placeholder="username"
+              />
+            </p>
+            <p>
+              <input
+                type="password"
+                autoComplete="off"
+                name="password"
+                placeholder="password"
+              />
+            </p>
           </div>
         ) : (
           <div>
-            <input type="text" name="username" placeholder="username" />
-            <input type="email" name="email" placeholder="email" />
-            <input type="password" name="password" placeholder="password" />
-            <input type="password" name="password2" placeholder="password" />
+            <p>
+              <input
+                type="text"
+                autoComplete="off"
+                name="username"
+                placeholder="username"
+              />
+            </p>
+            <p>
+              <input
+                type="email"
+                autoComplete="off"
+                name="email"
+                placeholder="email"
+              />
+            </p>
+            <p>
+              <input
+                type="password"
+                autoComplete="off"
+                name="password"
+                placeholder="password"
+              />
+            </p>
+            <p>
+              <input
+                type="password"
+                autoComplete="off"
+                name="password2"
+                placeholder="password check"
+              />
+            </p>
           </div>
         )}
-        <button>{loginForm ? "Login" : "Register"}</button>
-      </form>
+        <button>{loginForm ? "LOGIN" : "REGISTER"}</button>
+      </LoginForm>
       <button
         onClick={() => {
           setLoginForm(!loginForm);
         }}
       >
-        Login / Register
+        LOGIN / REGISTER
       </button>
-    </div>
+    </LoginDiv>
   );
 };
+
+const slidein = keyframes`
+  0%{
+    transform:translateY(100%);
+  }
+  90%{
+    transform:translateY(0%);
+  }
+`;
+
+const extend = keyframes`
+  0%{
+    height:0px;
+  }
+  100%{
+
+  }
+`;
+
+const LoginDiv = styled.div`
+  font-size: 32px;
+  font-weight: 400;
+  color: #333333;
+  display: inline-block;
+  background-color: white;
+  width: 400px;
+  height: 550px;
+  padding-top: 50px;
+  border-radius: 5px;
+  box-shadow: 10px 10px 10px 0px black;
+
+  animation: ${slidein} 2s ease-out;
+
+  > button,
+  > form button {
+    margin-top: 10px;
+    width: 300px;
+    height: 50px;
+    font-size: 24px;
+    font-weight: 300;
+    color: white;
+    border: none;
+    border-radius: 20px;
+    background: linear-gradient(
+      90deg,
+      rgba(220, 102, 255, 1) 0%,
+      rgba(88, 147, 255, 1) 100%
+    );
+  }
+`;
+
+const LoginForm = styled.form`
+  > div p input {
+    width: 300px;
+    height: 50px;
+    font-size: 24px;
+    background: #e5e5e5;
+    border: none;
+    border-radius: 3px;
+    padding: 0px 10px;
+    outline: none;
+    animation: ${extend} 0.5s ease-out;
+
+    :focus {
+      background-color: #d5d5d5;
+    }
+  }
+`;
 
 export default LoginContainer;
