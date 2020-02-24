eabc54481c2f12325094e9b3b8f47476c0cb5d8e
wow1548 <wow1548@naver.com>
2020-02-24 14:33:23 +0900

    create profile, message and chat models

--- app/chat/admin.py
+++ app/chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, Message, Chat
+
+
+admin.site.register(Profile)
+admin.site.register(Message)
+admin.site.register(Chat)
--- app/chat/models.py
+++ app/chat/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="profile")
+    avater = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="message")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.user.username
+
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(Profile, related_name="chats")
+    messages = models.ManyToManyField(Message, blank=True)
+
+    def last_10_messages(self):
+        return self.messages.order_by('-timestamp').all()[:10]
+
+    def __str__(self):
+        return "{}".format(self.pk)
