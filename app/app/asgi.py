fefa75489e3603ad8a1180b10aee750006dcb510
wow1548 <wow1548@naver.com>
2020-02-27 14:21:40 +0900

    settings

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -1,8 +1,8 @@
 import os
 import django
 from channels.routing import get_default_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import ASGIWHiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 django.setup()
-application = DjangoWhiteNoise(get_default_application())
+application = get_default_application()
